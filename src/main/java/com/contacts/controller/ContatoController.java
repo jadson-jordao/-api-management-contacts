8e55e6cbb3ddba17eedbf6f2aa7d7c91a8ec604d
jadson.silva <jadsonjordaosilva@hotmail.com>
2024-02-25 16:31:07 -0300

    Commit - Inclusão de vailidação nos DTOs e trativa de erros
    A exception lancada na validação da anotação @Valid é MethodArgumentNotValidException, desta forma, foi tratado uma mensagem de forma mais clara e objetiva ao usuario que estiver consumindo a API

--- src/main/java/com/contacts/controller/ContatoController.java
+++ src/main/java/com/contacts/controller/ContatoController.java
@@ -2,9 +2,9 @@ package com.contacts.controller;
 
 import com.contacts.dto.ContatoDTO;
 import com.contacts.entity.Contato;
-import com.contacts.entity.Endereco;
-import com.contacts.repository.ContatoRepository;
 import com.contacts.service.ContatoService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,10 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/v1/contatos")
@@ -24,30 +24,29 @@ public class ContatoController {
     @Autowired
     private ContatoService contatoService;
 
-    @Autowired
-    private ModelMapper modelMapper;
-
 
     @PostMapping
     public ResponseEntity<ContatoDTO> createContact(
-            @RequestBody
-            @Valid ContatoDTO contatoDTO){
-        ContatoDTO contact = contatoService.saveContato(contatoDTO);
+            @Valid
+            @RequestBody ContatoDTO contatoDTO){
+        ContatoDTO contact = contatoService.saveContact(contatoDTO);
         return new ResponseEntity<>(contact, HttpStatus.CREATED);
     }
 
     @GetMapping("/{idContato}" )
-    public ResponseEntity<ContatoDTO> getContactById(
+    public ResponseEntity<?> getContactById(
             @PathVariable("idContato") Long idContato){
-        Optional<Contato> contato = contatoService.findById(idContato);
-        return contato.map(value -> ResponseEntity.ok(modelMapper.map(value, ContatoDTO.class)))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        ContatoDTO contatoDTO = contatoService.findContactById(idContato);
+        if(Objects.isNull(contatoDTO))
+            return new ResponseEntity<>("Contato de identificador: "+idContato +" não cadastrado!", HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(contatoDTO, HttpStatus.OK);
     }
 
-    @GetMapping( )
+    @GetMapping
     public ResponseEntity<List<ContatoDTO>> getAllContacts(){
-        List<ContatoDTO> listContatos = contatoService.findAll();
-        return  ResponseEntity.ok(listContatos);
+        List<ContatoDTO> listContacts = contatoService.findAllContact();
+        return  ResponseEntity.ok(listContacts);
     }
 
     @DeleteMapping("/{idContato}" )
@@ -56,13 +55,38 @@ public class ContatoController {
         boolean isDeleted = contatoService.deleteContactById(idContato);
         if(isDeleted)
             return new ResponseEntity<>("Contado de identificador: "+ idContato + " deletado com sucesso!", HttpStatus.OK);
+
         return new ResponseEntity<>("Contato de identificador: "+idContato +" não cadastrado!", HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping( )
+    @DeleteMapping
     public ResponseEntity<String> deleteAllContact(){
-        contatoService.deleteAllContact();
-        return new ResponseEntity<>("Contatos deletados com sucesso!", HttpStatus.OK);
+        boolean isDeleted = contatoService.deleteAllContact();
+        if(isDeleted)
+            return new ResponseEntity<>("Contatos deletados com sucesso!", HttpStatus.OK);
+
+        return new ResponseEntity<>("Não existe contato a ser excluido!", HttpStatus.NOT_FOUND);
     }
 
+    @PutMapping("/{idContato}")
+    public ResponseEntity<?> updateContactPut(
+            @PathVariable("idContato") @NotNull Long idContato,
+            @Valid @RequestBody ContatoDTO contatoDTO){
+        ContatoDTO contatoAtualizado = contatoAtualizado = contatoService.updateContactPut(idContato, contatoDTO);
+        if(Objects.nonNull(contatoAtualizado))
+            return new ResponseEntity<>(contatoAtualizado, HttpStatus.OK);
+
+        return new ResponseEntity<>("Contato de identificador: "+idContato +" não cadastrado!", HttpStatus.NOT_FOUND);
+    }
+
+    @PatchMapping("/{idContato}")
+    public ResponseEntity<?> updateContactPatch(
+            @PathVariable("idContato") @NotNull Long idContato,
+            @Valid @RequestBody ContatoDTO contatoDTO){
+        ContatoDTO contatoAtualizado = contatoAtualizado = contatoService.updateContactPatch(idContato, contatoDTO);
+        if(Objects.nonNull(contatoAtualizado))
+            return new ResponseEntity<>(contatoAtualizado, HttpStatus.OK);
+
+        return new ResponseEntity<>("Contato de identificador: "+idContato +" não cadastrado!", HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/contacts/service/ContatoService.java
+++ src/main/java/com/contacts/service/ContatoService.java
@@ -3,15 +3,17 @@ package com.contacts.service;
 import com.contacts.dto.ContatoDTO;
 import com.contacts.entity.Contato;
 import com.contacts.repository.ContatoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class ContatoService {
 
     @Autowired
@@ -20,39 +22,106 @@ public class ContatoService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public ContatoDTO saveContato(ContatoDTO contatoDTO){
+    public ContatoDTO saveContact(ContatoDTO contatoDTO){
+        log.info("::Iniciando metodo de salvar contato.");
         Contato contato = modelMapper.map(contatoDTO, Contato.class);
-        contato.getEnderecos().stream().forEach(end-> end.setContato(contato));
+        contato.getEnderecos().forEach(end-> end.setContato(contato));
         contatoRepository.save(contato);
+        log.info("::Contato foi salvo com sucesso.");
         return modelMapper.map(contato,ContatoDTO.class);
     }
 
-    public ContatoDTO updateContato(ContatoDTO contatoDTO){
-        Contato contato = modelMapper.map(contatoDTO, Contato.class);
-        contatoRepository.save(contato);
+    public ContatoDTO updateContactPut(Long idContact, ContatoDTO contatoDTO){
+        log.info("::Iniciando metodo de atualização de contato pelo metodo PUT.");
+        Optional<Contato> contact = contatoRepository.findById(idContact);
+        if(!contact.isPresent())
+            return null;
+
+        Contato contato = contact.get();
+        contato.setNome(contatoDTO.getNome());
+        contato.setEmail(contatoDTO.getEmail());
+        contato.setDataNascimento(contatoDTO.getDataNascimento());
+        contato.setTelefone(contatoDTO.getTelefone());
+        contatoDTO.getEnderecos().forEach(
+                enderecoDTO -> contato.getEnderecos().stream()
+                        .filter(endereco -> Objects.equals(endereco.getIdEndereco(),enderecoDTO.getIdEndereco()))
+                        .forEach(endereco -> {
+                            endereco.setRua(enderecoDTO.getRua());
+                            endereco.setNumero(enderecoDTO.getNumero());
+                            endereco.setCep(enderecoDTO.getCep());
+                        })
+        );
+        Contato contatoAtualizado = contatoRepository.save(contato);
+        log.info("::Contato atualizado com sucesso pelo metodo PUT.");
+        return modelMapper.map(contatoAtualizado,ContatoDTO.class);
+    }
+    public ContatoDTO updateContactPatch(Long idContact, ContatoDTO contatoDTO){
+        log.info("::Iniciando metodo de atualização de contato pelo metodo PATCH.");
+        Optional<Contato> contact = contatoRepository.findById(idContact);
+        if(!contact.isPresent())
+            return null;
+
+        Contato contato = contact.get();
+        if(Objects.nonNull(contatoDTO.getNome()))
+            contato.setNome(contatoDTO.getNome());
+        if(Objects.nonNull(contatoDTO.getEmail()))
+            contato.setEmail(contatoDTO.getEmail());
+        if(Objects.nonNull(contatoDTO.getDataNascimento()))
+            contato.setDataNascimento(contatoDTO.getDataNascimento());
+        if(Objects.nonNull(contatoDTO.getTelefone()))
+            contato.setTelefone(contatoDTO.getTelefone());
+        contatoDTO.getEnderecos().forEach(
+                enderecoDTO -> contato.getEnderecos().stream()
+                        .filter(endereco -> Objects.equals(endereco.getIdEndereco(),enderecoDTO.getIdEndereco()))
+                        .forEach(endereco -> {
+                            if(Objects.nonNull(enderecoDTO.getRua()))
+                                endereco.setRua(enderecoDTO.getRua());
+                            if(Objects.nonNull(enderecoDTO.getNumero()))
+                                endereco.setNumero(enderecoDTO.getNumero());
+                            if(Objects.nonNull(enderecoDTO.getCep()))
+                                endereco.setCep(enderecoDTO.getCep());
+                        })
+        );
+        Contato contatoAtualizado = contatoRepository.save(contato);
+        log.info("::Contato atualizado com sucesso pelo metodo PATCH.");
+        return modelMapper.map(contatoAtualizado,ContatoDTO.class);
+    }
+
+    public ContatoDTO findContactById(Long idContato){
+        log.info("::Requisição do metodo GET, busca contato por id.");
+        Optional<Contato> contact = contatoRepository.findById(idContato);
+        if(!contact.isPresent())
+            return null;
+
+        Contato contato = contact.get();
+        log.info("::Requisição do metodo GET, busca contato por id executada com sucesso.");
         return modelMapper.map(contato,ContatoDTO.class);
     }
 
-    public Optional<Contato> findById(Long idContato){
-        return contatoRepository.findById(idContato);
-    }
-
-    public List<ContatoDTO> findAll(){
+    public List<ContatoDTO> findAllContact(){
+        log.info("::Requisição do metodo GET, busca todos os contatos cadastrados.");
         return contatoRepository.findAll().stream().
                 map(contato -> modelMapper.map(contato, ContatoDTO.class)).
                 collect(Collectors.toList());
     }
 
     public boolean deleteContactById(Long idContact){
+        log.info("::Requisição do metodo DELETE, deleta contato por id.");
         Optional<Contato> contact = contatoRepository.findById(idContact);
-        if(contact.isPresent()) {
-            contatoRepository.deleteById(idContact);
-            return true;
-        }
-        return false;
+        if(!contact.isPresent())
+            return false;
+        contatoRepository.deleteById(idContact);
+        log.info("::Requisição do metodo DELETE, exclusão por id realizada com sucesso.");
+        return true;
     }
 
-    public void deleteAllContact(){
-        contatoRepository.deleteAll();
+    public boolean deleteAllContact() {
+        log.info("::Requisição do metodo DELETE, deleta todos os contatos.");
+        if (contatoRepository.findAll().isEmpty())
+            return false;
+
+        contatoRepository.deleteAll();;
+        log.info("::Requisição do metodo DELETE, exclusão realizada com sucesso.");
+        return true;
     }
 }
